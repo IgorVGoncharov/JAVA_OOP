f556dfb8a767c76f4b0518383ad4a17b9440ed88
Igor <igorvgoncharov@mail.ru>
2023-03-08 19:32:22 +0300

    error corrected

--- HomeWork1/src/ManAndCupboard/Cupboard.java
+++ HomeWork1/src/ManAndCupboard/Cupboard.java
@@ -2,8 +2,6 @@ package ManAndCupboard;
 
 import java.util.ArrayList;
 
-import GeoTree.Sex;
-
 public class Cupboard {
     private String color;
     private Boolean dorOpen; // если true - то дверь открыта
